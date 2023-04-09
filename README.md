8f80a9b24a5ca39806188a5f711dfa5d74af5d0f
Narek <narek.khatchatrian@live.fr>
2023-04-09 16:40:01 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # boxinit.sh
- Quickly add a new entry in your /etc/hosts
+	Quickly add a new entry in your /etc/hosts.
+	I got tired of doing:
+	
+	> echo "10.10.X.X box.htb >> /etc/hosts"
+	
+	so I wrote this:
+	
+	> ./boxinit.sh 10.10.X.X box.htb
+
+# Usage
+	> chmod +x
+	> ./boxinit.sh <ip-address> <domain>
+
+# Exemple
+	> ./boxinit.sh 10.10.14.17 box.htb
+
+![It ain't much, but it's honest work](meme.jpg)
---
+++ boxinit.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Bored of manually adding a new box IP to your /etc/hosts?
+# Just use boxinit.sh [IP] [DOMAIN] !"
+
+BGreen='\033[1;32m'
+BRed='\033[1;31m'
+RESET='\033[0m'
+
+octet="(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])"
+ip4="^$octet\\.$octet\\.$octet\\.$octet$"
+
+function banner() {
+	echo "Usage:"
+	echo "	boxinit.sh [IP] [DOMAIN]"
+	echo -e "\nExample:"
+	echo "	boxinit.sh 10.10.13.37 superbox.local"
+}
+
+# check if any args is empty
+if [[ -z $1 || -z $2 ]]; then
+	echo -e "${BRed}Please provide an IP address and a domain.${RESET}\n"
+	banner
+	exit 1
+fi
+
+if [[ $1 =~ $ip4 ]]; then
+	echo "$1 $2" >> /etc/hosts
+	echo -e "${BGreen}[+]${RESET} Now resolving ${BGreen}$2${RESET} to ${BGreen}$1${RESET} !"
+	exit 0
+else
+        echo -e "${BRed}Incorrect IP address${RESET}\n"
+	banner
+	exit 1
+fi
